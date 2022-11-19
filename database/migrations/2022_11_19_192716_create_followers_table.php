1e243e27e7aa8b81b6c10b0e931e307b118fce7f
s3r0s4pi3ns <s3r0s4pi3ns@protonmail.com>
2022-11-19 21:17:10 +0100

    Default value for pivot value on followers table

--- database/migrations/2022_11_19_192716_create_followers_table.php
+++ database/migrations/2022_11_19_192716_create_followers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('followers', function (Blueprint $table) {
             $table->foreignId('follower_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('following_id')->constrained('users')->cascadeOnDelete();
-            $table->boolean('accepted')->false();
+            $table->boolean('accepted')->default(false);
             $table->timestamps();
         });
     }
